e4735aa5d76781763bd46fe403f42848e3621a3e
Jasmeet Singh <jasmeetsingh0918@gmail.com>
2022-05-05 11:15:24 +0530

    Second commit to creash course

---
+++ Express js Crash Course/Members.js
@@ -0,0 +1,25 @@
+// Members api...
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "jdoe@gmail.com",
+    status: "active",
+  },
+
+  {
+    id: 2,
+    name: "Bob Williams",
+    email: "bwills@gmail.com",
+    status: "inactive",
+  },
+
+  {
+    id: 3,
+    name: "Steve Smith",
+    email: "smithSteve21@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
